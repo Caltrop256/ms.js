ea0200776267be5b19197587edfe6be9f01c26f8
Caltrop <47452720+CaltropUwU@users.noreply.github.com>
2019-09-08 14:00:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,142 @@
+'use strict';
+const matchSorter = require('match-sorter').default;
+
+function plural(str, count) {
+    return `${str}${count == 1 ? '' : 's'}`;
+};
+
+class Unit {
+    constructor(name, definition, aliases) {
+        this.name = name;
+        this.definition = definition;
+        this.aliases = aliases;
+    };
+};
+
+class Time {
+    constructor(ms, opts) {
+        const round = ms > 0 ? Math.floor : Math.ceil;
+
+        this.eternity      =     (ms === Infinity) ? 1 : 0;
+        ms = this.eternity ? 0 : ms;
+        this.aeon          =     round(ms / 3.1536e+19)
+        this.millenium     =     round(ms / 3.1536e+13) % 1000000;
+        this.century       =     round(ms / 3.1536e+12 % 10);
+        this.decade        =     round(ms / 3.1536e+11) % 10;
+        this.year          =     round(ms / 3.1536e+10) % 10;
+        this.day           =     round(ms / 86400000) % 365;
+        this.hour          =     round(ms / 3600000) % 24;
+        this.minute        =     round(ms / 60000) % 60;
+	    this.second        =     round(ms / 1000) % 60;
+	    this.millisecond   =     round(ms) % 1000;
+	    this.microsecond   =     round(ms * 1000) % 1000;
+        this.nanosecond    =     round(ms * 1e6) % 1000;
+
+        let str = '';
+        let int =  0;
+        let firstRelevant;
+
+        for(const t in this) {
+            let unit = units.find(u => u.name == t)
+            if(this[t] && (!firstRelevant || int < firstRelevant + opts.relevant)) {
+                str += `${this[t]}${opts.verbose ? " " + plural(t, this[t]) : unit.aliases[0]}, `;
+                if(!firstRelevant) firstRelevant = int;
+            };
+            int++;
+        };
+        this.ms = ms;
+
+        this.str = str.replace(/, ([^,]*)$/, '').replace(/, ([^,]*)$/, `${opts.verbose ? ` and $1` : ', $1'}`);
+        this.toString = function() {
+            return this.str;
+        };
+        this.valueOf = function() {
+            return this.ms;
+        }
+    };
+};
+const units = [
+    new Unit('eternity', Infinity, ['eternity', 'eternities']),
+    new Unit('aeon', 3.1536e+19, ['ae','æ', 'aeons', 'eon', 'eons']),
+    new Unit('millenium', 3.1536e+13, ['ml', 'milleniums', 'millennia']),
+    new Unit('century', 3.1536e+12, ['l', 'cent', 'centurys', 'centuries']),
+    new Unit('generation', 1103760000000, ['generations']),
+    new Unit('decade', 3.1536e+11, 'dc', 'decades'),
+    new Unit('megaminute', 60000000000, ['megaminutes']),
+    new Unit('year', 3.1536e+10, ['y', 'yr', 'yrs', 'years']),
+    new Unit('season', 7884086400, ['quarter', 'trimonth']),
+    new Unit('month', 2628028800, ['mo', 'mon', 'months']),
+    new Unit('lunar month', 2449440000, ['lunarmonth', 'lunarmonths']),
+    new Unit('fortnight', 1209600000, ['biweek', 'fortnights', 'biweeks']),
+    new Unit('week', 604800000, ['w', 'weeks']),
+    new Unit('day', 86400000, ['d', 'days']),
+    new Unit('hour', 3600000, ['h', 'hr', 'hrs', 'hours']),
+    new Unit('moment', 90000, ['moments']),
+    new Unit('minute', 60000, ['m', 'mn', 'min', 'mins', 'minutes']),
+    new Unit('instant', 8000, ['in', 'instants']),
+    new Unit('second', 1000, ['s', 'sec', 'secs', 'seconds']),
+    new Unit('millisecond', 1, ['ms', 'msec', 'msecs', 'milliseconds']),
+    new Unit('microsecond', 0.001, ['µs', 'micro', 'micros', 'microsec', 'microsecs','microseconds']),
+    new Unit('nanosecond', 0.000001, ['ns', 'nano', 'nanos', 'nanosec', 'nenosecs','nanoseconds'])
+];
+
+function timeFromString(str = '', opts) {
+    str = str.replace(/[^a-z0-9.\-]+/g, '').toLowerCase();
+    var ms = 0;
+    var cycle = 0;
+    while(str.length) {
+
+        cycle++;
+        if(cycle > 10) break;
+
+        var sepArg = str.match(/[a-zæ]+|[0-9.\-]+/gi);
+        if(!sepArg) return void 0;
+        if(sepArg.length == 1) {
+            if(opts.strictMode) return void 0;
+
+            if(sepArg[0].toString().match(/[a-zæ]+/gi)) {
+                sepArg[1] = sepArg[0];
+                sepArg[0] = "1";
+            } else {
+                sepArg[1] = "m";
+            };
+        };
+        if(sepArg[1].match(/[^a-zæ]/gi) || sepArg[0].match(/[^0-9.\-]/g)) continue;
+
+        var from = units.find(u => u.name == sepArg[1] || u.aliases.includes(sepArg[1]));
+        if(!from) {
+            let uNames = [];
+            units.forEach(u => {
+                uNames.push(...[u.name, ...u.aliases]);
+            });
+            var matches = matchSorter(uNames, sepArg[1]);
+            if(!matches.length) continue;
+            var from = units.find(u => u.name == matches[0] || u.aliases.includes(matches[0]));
+        };
+        ms += parseInt(sepArg[0]) * from.definition;
+        str = str.substr(sepArg[0].length + sepArg[1].length);
+    };
+    if(!ms) return void 0;
+    return new Time(ms, opts);
+};
+
+function timeFromNumber(ms, opts) {
+    return new Time(ms, opts);
+};
+
+function time(input, options) {
+    if(!options) options = {};
+    if(options.verbose == undefined) options.verbose = true;
+    const opts = {
+        verbose: !!options.verbose,
+        relevant: options.relevant || 2,
+        strictMode: !!options.strict
+    };
+    if(typeof input == 'number') {
+        return timeFromNumber(input, opts);
+    } else if(typeof input == 'string') {
+        return timeFromString(input, opts);
+    } else return void 0;
+};
+
+module.exports = time;
---
+++ readme.md
@@ -0,0 +1,128 @@
+#ms.js
+
+A simple utility module which will help you implement time-based applications.
+
+##Behaviour
+
+**The function will always return a `Time` Object.**
+
+For example:
+```js
+const time = require('ms.js');
+console.log(time(5200));
+```
+will return:
+```js
+{
+    eternity: 0,
+    aeon: 0,
+    century: 0,
+    decade: 0,
+    year: 0,
+    day: 0,
+    hour: 0,
+    minute: 0,
+    second: 5,
+    millisecond: 200,
+    microsecond: 0,
+    nanosecond: 0,
+    
+    ms: 5200,
+    str: '5 seconds and 200 milliseconds'
+}
+```
+
+Invoking the `toString` function will always return the `.str` value.
+For example:
+```js
+const time = require('ms.js');
+console.log(`Shutting down in ${time(5200)}`); // => 'Shutting down in 5 seconds and 200 milliseconds'
+```
+
+Invoking the `valueOf` function will always return the `.ms` value.
+For example:
+```js
+const time = require('ms.js');
+time(5200) + 300; // => 5500
+```
+
+##Input
+
+Valid Input is always either a `Number` or a `String`!
+
+###Examples:
+```js
+const time = require('ms.js');
+
+time(1000).str;                            // => '1 second'
+time(1000 * 60).str;                       // => '1 minute'
+time('2 hours').ms;                        // => 7200000
+time('50y, 30h, 20m, 10s').str;            // => '5 decades, 1 day, 6 hours, 20 minutes and 10 seconds'
+time('30 hours, 20 min10seconds5 0y').str; // => '5 decades, 1 day, 6 hours, 20 minutes and 10 seconds'
+time(time('2 hours') + time(10000)).str;   // =>  '2 hours and 10 seconds'
+time('-3 days').ms;                        // => -259200000
+time('20').str;                            // => '20 minutes'
+time(20).str;                              // => '20 milliseconds'
+time(2.5).str;                             // => '2 milliseconds and 500 microseconds'
+time('2.5 hours').str;                     // => '2 hours and 30 minutes'
+time('minute').ms                          // => 60000
+time(Infinity).str                         // => '1 eternity'
+```
+(assuming that all relevant values will be shown, see: "**Options**" section)
+
+###String input Units
+
+Unit Name | Definition
+---|---
+Eternity | An Infinite amount of time
+Aeon | `3.1536e+19 ms` or 1 billion years
+Millenium | `3.1536e+13 ms` or 1,000 years
+Century | `3.1536e+12 ms` or 100 years
+Generation | `1103760000000 ms` or 3 decades and 5 years
+Decade | `3.1536e+11 ms` or 10 years
+Megaminute | `60000000000 ms` or 1,000,000 minutes or about 1 year and 329 days
+Year | `3.1536e+10 ms` or 365 days
+Season | `7884086400 ms` or about 91 days and 6 hours
+Month | `2628028800 ms` or about 30 days and 10 hours
+Lunar Month | `2449440000 ms` or about 28 days and 8 hours
+Fortnight | `1209600000 ms` or 14 days
+Week | `604800000 ms` or 7 days
+Day | `86400000 ms` or 24 hours
+Hour | `3600000 ms` or 60 minutes
+Moment | `90000 ms` or 1 minute and 30 seconds
+Minute | `60000 ms` or 60 seconds
+Instant | `8000 ms` or 8 seconds
+Second | `1000 ms`
+Millisecond | `1 ms`
+Microsecond | `0.001 ms`
+Nanosecond | `0.000001 ms` or 0.001 microseconds
+
+###Options
+
+Options are parsed in an Object as the 2nd parameter to the function.
+
+Option Name | Purpose | Default
+---|---|---
+verbose | Whether or not to show the entire Unit name or to use a shortened name | `true`
+relevant | How many relevant Units to display in the String | `2`
+strict | Whether or not to limit the use of incomplete input for String input | `false`
+
+**Examples:**
+```js
+const time = require('ms.js');
+
+time(55555, {verbose: true}).str    // => '55 seconds and 555 milliseconds'
+time(55555, {verbose: false}).str   // => '55s, 555ms'
+
+time(55555555, {relevant: 2}).str   // => '15 hours and 25 minutes'
+time(55555555, {relevant: 3}).str   // => '15 hours, 25 minutes and 55 seconds'
+time(55555555, {relevant: 4}).str   // => '15 hours, 25 minutes, 55 seconds and 555 milliseconds'
+
+time('minute', {strict: false}).str // => '1 minute'
+time('1', {strict: false}).str      // => '1 minute'
+time('minute', {strict: true}).str  //TypeError: Cannot read property 'str' of undefined
+time('1', {strict: true}).str       //TypeError: Cannot read property 'str' of undefined
+
+
+time('66666', {strict: false, verbose: false, relevant: 99}) // => 46d, 7h, 6m
+```
